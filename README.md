e30acb1e8959d10b53bf7cba986cac844951a9cf
zerobit <orourke_kyle@icloud.com>
2020-08-25 18:05:09 -0400

    skeleton code for db connection

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # employeeTracker
+
+#requirements
+-view all employees
+-view departments
+-view roles
+-add/remove employee
+-update employee roles
--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,17 @@
 // establish connection to db
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+
+    password: "",
+    database: "employees"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+    connection.end();
+});
